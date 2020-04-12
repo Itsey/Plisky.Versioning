f50f018ccf430d751082d87a729c8c1c6bb66720
Jim <itsey@plisky.net>
2020-04-12 15:15:16 +0100

    Versioning Release Script Update

--- Versioning/_Dependencies/CDSupport/CD-UnicornRelease.ps1
+++ Versioning/_Dependencies/CDSupport/CD-UnicornRelease.ps1
@@ -1,1 +1,1 @@
-﻿copy-item -path .\Versioning\PliskyTool\bin\Release\netcoreapp3.1\ -destination c:\Files\BuildTools\PliskyVersioner\
+﻿copy-item -path .\Versioning\PliskyTool\bin\Release\netcoreapp3.1 -destination c:\Files\BuildTools\PliskyVersioner -force
--- Versioning/_Dependencies/SupportingFiles/testing.cmd
+++ Versioning/_Dependencies/SupportingFiles/testing.cmd
@@ -6,8 +6,11 @@ pushd C:\Files\Code\git\PliskyVersioning\Versioning\PliskyTool\bin\Debug\netcore
 :: Test no version store
 ::dotnet PliskyTool.dll UpdateFiles -DryRun -Root=c:\files\code\git\pliskyversioning\versioning\testdata -VS=c:\temp\toolver.ver -Increment
 
-dotnet PliskyTool.dll Override -VS=c:\temp\toolver.ver -Q=.+.Alpha.0 
+::dotnet PliskyTool.dll Override -VS=c:\temp\toolver.ver -Q=.+.Alpha.0 
 ::-DryRun
+
+
+::dotnet PliskyTool.dll CreateVersion -VS=\\unicorn\Files\BuildTools\VersionStore\pversioner.vstore -Q=1.0.0.0
 
 popd
 
