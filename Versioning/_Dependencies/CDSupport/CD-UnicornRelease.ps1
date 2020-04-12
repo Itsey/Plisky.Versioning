0c62248b50f6851625eb5f3a0e3372437de9f035
Jim <itsey@plisky.net>
2020-04-12 23:29:04 +0100

    Updating files

--- Versioning/_Dependencies/CDSupport/CD-UnicornRelease.ps1
+++ Versioning/_Dependencies/CDSupport/CD-UnicornRelease.ps1
@@ -1,1 +1,2 @@
-﻿copy-item -path .\Versioning\PliskyTool\bin\Release\netcoreapp3.1 -destination c:\Files\BuildTools\PliskyVersioner -force
+﻿ copy-item -path .\Versioning\PliskyTool\bin\Release\netcoreapp3.1\*.* -destination c:\Files\BuildTools\PliskyVersioner\ -force
+
